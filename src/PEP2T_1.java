0f64c00e49bbf0cc2945e32cba7d31dd6cf604d1
Guillermo <gremesalgarcia@gmail.com>
2023-12-21 21:36:28 +0100

    Primeros pasos

--- src/PEP2T_1.java
+++ src/PEP2T_1.java
@@ -1,5 +1,20 @@
+import static utiles.Utilidades.*;
+
 public class PEP2T_1 {
     public static void main(String[] args) {
+        print("1) Retirar");
+        print("2) Ingreso de dinero");
+        print("3) Salir");
+        char opcion = (char) input("Opción: ");
+        float saldo = Float.parseFloat(args[0]);
+        print("Su saldo actuales de %s", saldo);
+        int retirar = (int) inputfloat("Itroduzca lo que desea retirar%n");
+        Retirada(retirar,saldo,Integer.parseInt(args[1]));
+    }
+    public static int Retirada (int dineroRet,float disponible, int tope){
+        print("Su saldo actual es de %s", disponible);
 
+        print("%s %s %s", dineroRet, disponible, tope);
+        return 10;
     }
 }
