24051c41a1edc0c23a0305bc4c580df758fc3680
Guillermo <gremesalgarcia@gmail.com>
2024-01-09 22:00:20 +0100

    Comentarios + Refactorizacion

--- src/PEP2T_1_GRG.java
+++ src/PEP2T_1_GRG.java
@@ -1,33 +1,42 @@
 import java.util.Scanner;
 
 public class PEP2T_1_GRG {
+    // Creacion de los atributos para el metodo constructor
     float saldo;
     float tope;
-    PEP2T_1_GRG(float saldo1, float tope1){
+
+    // Creacion de metodo constructor
+    private PEP2T_1_GRG(float saldo1, float tope1){
         saldo = saldo1;
         tope = tope1;
     }
 
+    // Metodo main (metodo inicial, contiene el menu de inicio
     public static void main (String[] args) {
+        // Creacion de un objeto de Scanner para que el cliente pueda introducir datos
         Scanner input = new Scanner(System.in);
+        // Creacion de objeto cuyo nombre va a ser datos1
         PEP2T_1_GRG datos1 = new PEP2T_1_GRG(Float.parseFloat(args[0]), Float.parseFloat(args[1]));
+
         boolean salir = false;
         while (!salir) {
-
+            // Menu de inicio
             System.out.println("\t\t\t\t" + "Menú de opciones");
             System.out.println("\t\t\t\t" + "================\n");
             System.out.println("\t" + "1) Retirada de dinero");
             System.out.println("\t" + "2) Ingreso de dinero");
             System.out.println("\t" + "3) Salir");
 
+            // Pedir al cliente que ingrese lo que desea hacer
             System.out.print("\n\t\t\t\t" + "Opción: ");
             int opcion = input.nextInt();
             System.out.println();
 
+            // Eleccion de opciones
             System.out.printf("\t\tSu saldo actual es de %s €\n", datos1.saldo);
             switch (opcion) {
                 case 1:
-                    datos1.retirada();
+                    datos1.verificacionTope();
                     break;
                 case 2:
                     datos1.ingresos();
@@ -41,41 +50,48 @@ public class PEP2T_1_GRG {
         }
     }
 
-    void verificacionTope (float retirar) {
-        if (retirar < tope && retirar < saldo) {
+    // Creacion de metodos para la verificacion de los limites y pedir los datos al cliente
+    private void verificacionTope () {
+        Scanner input = new Scanner(System.in);
+        System.out.print("\t\tTeclee dinero a retirar:\n\t\t");
+        float retirar = input.nextFloat();
+        if (retirar < tope) {
             verificacionDinero(retirar);
+            System.out.printf("\t\tUsted retiró %s €\n", retirar);
         }
         else
         {
+            System.out.printf("\t\tIntenta retirar %s € \n", retirar);
             System.out.printf("\t\tTiene establecido ahora un tope de %3.0f€ \n", tope);
         }
     }
 
-    void verificacionDinero (float retirar){
+    private void verificacionDinero (float retirar){
         if (retirar < saldo){
-            saldo -= retirar;
-            tope -= retirar;
+            retirada(retirar);
         }
         else
         {
+            System.out.printf("\t\tIntenta retirar %s € \n", retirar);
             System.out.println("No tiene el suficiente dinero en la cuenta, revise sus ingresos");
         }
     }
 
-    void retirada (){
-        Scanner input = new Scanner(System.in);
-        System.out.print("\t\tTeclee dinero a retirar:\n\t\t");
-        float retirar = input.nextFloat();
-        System.out.printf("\t\tIntenta retirar %s € \n", retirar);
-        verificacionTope(retirar);
+    // Metodo para hacer la retirada
+    private void retirada (float retirar){
+        saldo -= retirar;
+        tope -= retirar;
     }
 
-    void ingresos (){
+    // Metodos para hacer el ingreso de dinero
+    private void ingresos (){
         Scanner input = new Scanner(System.in);
+
         System.out.print("\t\tTeclee dinero a ingresar:\n\t\t");
         float ingresar = input.nextFloat();
-        System.out.printf ("\t\tUsted ingresó %s €\n", ingresar);
+
         saldo += ingresar;
+        System.out.printf ("\t\tUsted ingresó %s €\n", ingresar);
     }
 
 }
